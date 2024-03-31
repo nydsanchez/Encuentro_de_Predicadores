a0222ba459a239b1fe153deb5524be431b26da35
Nydia Massiel Sánchez <66643813+nydsanchez@users.noreply.github.com>
2024-03-30 21:14:31 -0600

    idc

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const PORT = 4000;
 conn
   .sync({ force: true })
   .then(() => {
-    server.listen(PORT, () => {
+    server.listen(PORT, "0.0.0.0", () => {
       console.log(`Server listening on port ${PORT}`);
     });
   })
