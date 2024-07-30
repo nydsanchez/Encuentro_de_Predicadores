4f33b5f59ffa6c4d048191fe3bfa01ebba42da1e
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-07-29 18:25:54 -0600

    Update Tickets.js

--- Server/src/models/Tickets.js
+++ Server/src/models/Tickets.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
     },
 
     state_ticket: {
-      type: DataTypes.ENUM("reservado", "utilizado", "pagado"),
+      type: DataTypes.ENUM("reservado", "utilizado"),
       allowNull: false,
       defaultValue: "reservado",
     },
