a614f505dd302d5acec82b89c25b084a494a112c
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-07-31 17:54:23 -0600

    chore: update header component

--- Client/src/components/header/Header.jsx
+++ Client/src/components/header/Header.jsx
@@ -1,26 +1,24 @@
-import { format } from "date-fns";
-import esLocale from "date-fns/locale/es";
+import Logo from "./logo";
 
 import styles from "./header.module.css";
 
 const Header = () => {
-  let today = new Date();
-  const formattedDate = format(today, "EEEE, d MMMM yyyy", {
-    locale: esLocale,
-  });
-  const year = today.getFullYear();
-
   return (
     <header className={`${styles.header} fixed-top w-100 bg-light`}>
-      <div className={styles.imgLogoBox}>
-        <img className={styles.imgLogo} src="/logo-v2.png" alt="logo" />
-      </div>
+      <Logo />
+
       <div className={styles.headerText}>
-        <h1 data-date={formattedDate}>Encuentro de Predicadores {year}</h1>
+        <h1>Encuentro de Predicadores</h1>
         <p>Iglesia de Cristo en Nicaragua | Evento presencial</p>
-        <p>{formattedDate}</p>
       </div>
-      <div></div>
+
+      <div>
+        <img
+          className={styles.imgLogo2}
+          src="/logoHeadName.png"
+          alt="logo SC - Consulting"
+        />
+      </div>
     </header>
   );
 };
