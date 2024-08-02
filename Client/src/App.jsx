278b09b0c08d7e6706c84c05a7485a4c89df11a6
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-08-01 18:44:42 -0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "./Pages/notFound/PageNotFound";
 import Home from "./Pages/Home/Home";
-import NewChurches from "./Pages/Churches/NewChurches";
-import ChurchesList from "./Pages/Churches/ChurchesList";
-import NewPerson from "./Pages/People/NewPerson";
-import PeopleList from "./Pages/People/PeopleList";
-import NewTicket from "./Pages/Tickets/NewTicket";
-import TicketList from "./Pages/Tickets/TicketList";
+
 import Registro from "./Pages/registro";
+import Consultas from "./Pages/consultas";
+import Asistencia from "./Pages/asistencia";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="*" element={<PageNotFound />} />
-      <Route path="/registro/" element={<Registro />} />
-      <Route path="/registro/congregacion" element={<NewChurches />} />
-      <Route path="/registro/persona" element={<NewPerson />} />
-      <Route path="/registro/ticket" element={<NewTicket />} />
-      <Route path="/registro/persona-ticket" element={<NewTicket />} />
 
-      <Route path="/iglesia/listado" element={<ChurchesList />} />
-      <Route path="/personas/listado" element={<PeopleList />} />
-      <Route path="/tickets/listado" element={<TicketList />} />
+      <Route path="/registro" element={<Registro />} />
+      <Route path="/consultas" element={<Consultas />} />
+      <Route path="/confirmar-asitencia" element={<Asistencia />} />
+      <Route path="/rifa" element={<Asistencia />} />
     </Routes>
   );
 }
